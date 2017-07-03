e329bfad9944b3be07f98ee462d6160405e6dc0f
yl573 <yl573@cam.ac.uk>
2017-07-03 22:55:09 +0100

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='catalearn',
-    version='0.1',
-    packages=find_packages("."),
-    url='https://github.com/Pithikos/python-websocket-server',
-    install_requires=[
-    ],
-    platforms='any',
-)
+setup(name='catalearn',
+      version='0.1',
+      description='A module for running machine learning code on cloud GPUs',
+      url='https://github.com/Catalearn/catalearn',
+      author='Edward Liu',
+      author_email='edwardliu573@gmail.com',
+      license='MIT',
+      packages=['catalearn'],
+      zip_safe=False)
